4023c0801c4fdae6c086c41119a065e3e9cb7956
Yevgeniy.Aleksandrov <Yevgeniy.Aleksandrov@nexign.com>
2020-01-17 16:09:46 +0300

    loyalty-2: Removed Article.kt ArticleRepository.kt end Extensions.kt

--- src/main/kotlin/com/evg/repository/ArticleRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.evg.repository
-
-import com.evg.entity.Article
-import org.springframework.data.repository.CrudRepository
-
-interface ArticleRepository : CrudRepository<Article, Long> {
-    fun findBySlug(slug: String): Article?
-    fun findAllByOrderByAddedAtDesc(): Iterable<Article>
-}
--- src/test/kotlin/com/evg/Repositories.kt
+++ src/test/kotlin/com/evg/Repositories.kt
@@ -1,32 +1,17 @@
 package com.evg
 
-import com.evg.entity.Article
 import com.evg.entity.User
-import com.evg.repository.ArticleRepository
 import com.evg.repository.UserRepository
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
-import org.springframework.data.repository.findByIdOrNull
 
 @DataJpaTest
 class RepositoriesTests @Autowired constructor(
     val entityManager: TestEntityManager,
-    val userRepository: UserRepository,
-    val articleRepository: ArticleRepository) {
-
-    @Test
-    fun `When findByIdOrNull then return Article`() {
-        val juergen = User("springjuergen", "Juergen", "Hoeller")
-        entityManager.persist(juergen)
-        val article = Article("Spring Framework 5.0 goes GA", "Dear Spring community ...", "Lorem ipsum", juergen)
-        entityManager.persist(article)
-        entityManager.flush()
-        val found = articleRepository.findByIdOrNull(article.id!!)
-        assertThat(found).isEqualTo(article)
-    }
+    val userRepository: UserRepository) {
 
     @Test
     fun `When findByLogin then return User`() {
